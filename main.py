5e0229a68c5474c825006c0f1c252d29a05375f5
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-04-08 22:52:03 +0100

    configurate main file

---
+++ main.py
@@ -0,0 +1,21 @@
+from modules.pipeline import ArabicNLPPipeline
+
+def main():
+    # Configuration
+    config = {
+        "stop_words_file": "data/stop_arabic.txt",
+        "pdf_directory": "data/pdf_files/",
+        "corpus_file": "data/corpus.txt",
+        "phrases_file": "data/phrases.json",
+        "vocab_0_file": "data/vocabulary_0.json",
+        "vocab_1_file": "data/vocabulary_1.json",
+        "word_freq_0_file": "data/word_freq_0.json",
+        "word_freq_1_file": "data/word_freq_1.json"
+    }
+
+    # Create and run pipeline
+    pipeline = ArabicNLPPipeline(config)
+    pipeline.run()
+
+if __name__ == "__main__":
+    main()
