c299aec07fa6192bd2719d2885ffcc791bf6eec1
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-04-08 22:41:42 +0100

    toknizer words

--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -3,21 +3,9 @@ import re
 
 class ArabicTokenizer:
     def is_arabic(self, word):
-        """
-        Check if a word is entirely in Arabic.
-
-        :param word: Input word.
-        :return: True if it's Arabic, False otherwise.
-        """
         return bool(re.fullmatch(r'[\u0600-\u06FF]+', word))
 
     def tokenize(self, text):
-        """
-        Tokenize Arabic text and remove invalid words.
-
-        :param text: Input Arabic text.
-        :return: List of Arabic words.
-        """
         # Match Arabic words of at least 2 letters
         words = re.findall(r'\b[ء-ي]{2,}\b', text)
 
